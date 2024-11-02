a8d386cc7ed819b2c8a32abcad99cd5262ab28ee
Cian O'Gralaigh <ihatecian@gmail.com>
2024-11-02 12:11:58 +0000

    new: add rendering for home app

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import "./index.scss";
+import ReactDOM from "react-dom";
 
-import './index.scss'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      Home page Content
+    </div>
+    <Footer />
   </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+);
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
-
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById("app"));
